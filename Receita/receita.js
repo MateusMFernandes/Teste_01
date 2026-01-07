0747fb6cf883e7df4f68d716e16c04aa18923e2c
Mateus Moura Fernandes <111711553+MateusMFernandes@users.noreply.github.com>
2026-01-06 22:51:45 -0300

    at

--- Receita/receita.js
+++ Receita/receita.js
@@ -1,15 +1,6 @@
 // ano automático
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// copiar receita
-document.getElementById("btnCopy").addEventListener("click", () => {
-  const text = document.getElementById("recipe-text").innerText;
-
-  navigator.clipboard.writeText(text).then(() => {
-    alert("Receita copiada com sucesso!");
-  });
-});
-
 // compartilhar
 document.getElementById("btnShare").addEventListener("click", () => {
   const title = document.getElementById("recipe-title").innerText;
